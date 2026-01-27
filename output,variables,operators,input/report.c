f222ad15360b5c7936b95f7199e5bd9fc16c27b4
Vibha Manomai <25wh1a66c4@bvrithyderabad.edu.in>
2026-01-27 19:12:24 +0530

    output,variables,operators,input

---
+++ output,variables,operators,input/report.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float m1 = 100;
+    float m2 = 98;
+    float m3 = 96;
+    float m4 = 94;
+    float m5 = 92;
+    float p = (m1+m2+m3+m4+m5)/5;
+    printf("Percentage of 5 subjects is : %f",p);
+    return 0;
+
+}
---
+++ output,variables,operators,input/sum.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a = 30;
+    int b = 50;
+    int sum = a + b;
+    printf("Sum is %d",sum);
+    return 0;
+}
